87e86e4aefb88ce356ce712d769dd481847cd27d
Cody Biller <sharpeleven91@gmail.com>
2019-03-24 21:43:34 -0400

    update statbars styling

--- src/components/UI/Status/StatBars.js
+++ src/components/UI/Status/StatBars.js
@@ -3,34 +3,41 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
+  width: 200px;
+  color: white;
+  font-weight: bold;
   height: fit-content;
+  justify-content: space-around;
+  align-items: center;
   flex-direction: column;
-  * : {
-    padding: 5px;
-  }
 `;
 const HealthBar = styled.div`
   background-color: #660000;
   display: flex;
+  margin-bottom: 5px;
+  align-items: center;
   justify-content: center;
-  height: 10px;
-  width: 300px;
+  height: 20px;
+  width: 80%;
   border-radius: 5px;
 `;
 const ManaBar = styled.div`
   background-color: #274c77;
   display: flex;
+  margin-bottom: 5px;
+  align-items: center;
   justify-content: center;
-  height: 10px;
-  width: 300px;
+  height: 20px;
+  width: 80%;
   border-radius: 5px;
 `;
 const StaminaBar = styled.div`
   background-color: #f0b323;
   display: flex;
+  align-items: center;
   justify-content: center;
-  height: 10px;
-  width: 300px;
+  height: 20px;
+  width: 80%;
   border-radius: 5px;
 `;
 function StatBars() {
