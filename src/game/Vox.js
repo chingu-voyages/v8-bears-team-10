dec9147ec92a4bab761970a07a64c4429cd2f698
Xevoxe <xevoxe@gmail.com>
2019-03-27 20:02:25 -0500

    Changed ConnectionManager to WorldManager

--- src/game/Vox.js
+++ src/game/Vox.js
@@ -1,10 +1,10 @@
 const GM = require('./GameManager');
 const Server = require("./Server");
-const ConnectionManager = require("./ConnectionManager");
+const WorldManager = require("./WorldManager");
 
 GM.Start()
 
 let server1 = new Server(1,3000,GM);
-let cm = new ConnectionManager(GM);
+let cm = new WorldManager(GM);
 server1.Start();
 cm.Start();
